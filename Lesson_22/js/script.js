119170ec72e7011c7f7cbcd75c60e239312f0c3b
ola-laaa <112777490+ola-laaa@users.noreply.github.com>
2022-12-02 23:41:39 +0200

    Add files via upload

---
+++ Lesson_22/js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+console.log("Hello world!");
+let userEyeColor;
+let user;
+let userName = "Вася";
+user = userName;
+console.log(user);
+let userAge = 36;
+let userInfo = `Фрилансер ${userAge}`;
+console.log(userInfo);
